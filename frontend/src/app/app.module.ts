7323b9321b59ce3ff56c86ae4725ee7352dd5c80
gabriellimad <94320456+gabriellimad@users.noreply.github.com>
2022-08-29 08:36:40 -0300

    ATT service

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -17,7 +17,7 @@ import { RedDirective } from './directives/red.directive';
 import { ForDirective } from './directives/for.directive';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 
 @NgModule({
@@ -40,7 +40,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatSidenavModule,
     MatListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
